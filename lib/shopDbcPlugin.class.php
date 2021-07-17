a915727e427e6b50480e6475a98d7e5d08c8700e
Serge Rodovnichenko <sergerod@gmail.com>
2021-07-17 11:59:39 +0300

    Системные требования и docblock

--- lib/shopDbcPlugin.class.php
+++ lib/shopDbcPlugin.class.php
@@ -1,5 +1,14 @@
 <?php
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @copyright Serge Rodovnichenko, 2021
+ * @license Webasyst
+ */
+declare(strict_types=1);
 
+/**
+ * Main plugin class
+ */
 class shopDbcPlugin extends shopPlugin
 {
 
