c3842bdaccb249ce69a9e478711fc9d7dcfd1280
Serge Rodovnichenko <sergerod@gmail.com>
2021-07-17 13:25:23 +0300

    Настройка и подготовка настройки со способами доставки и оплаты

--- lib/actions/settings/shopDbcPluginSettings.action.php
+++ lib/actions/settings/shopDbcPluginSettings.action.php
@@ -11,5 +11,70 @@ declare(strict_types=1);
  */
 class shopDbcPluginSettingsAction extends waViewAction
 {
+    public function execute()
+    {
+        /** @var shopDbcPlugin $plugin */
+        $plugin = wa('shop')->getPlugin('dbc');
 
+        $info = [
+            'plugin_name'    => $plugin->getName(),
+            'plugin_version' => $plugin->getVersion(),
+        ];
+
+        $settings = $plugin->getSettings();
+        $settings['payment'] = $this->actualizeSelected($settings['payment'], $this->listMethods('payment'));
+        $settings['shipping'] = $this->actualizeSelected($settings['shipping'], $this->listMethods('shipping'));
+
+        $this->view->assign(compact('info', 'settings'));
+    }
+
+    /**
+     * Список методов доставки или оплаты в магазине
+     *
+     * @param string $type 'shipping' или 'payment'
+     * @return array
+     * @throws waException
+     */
+    protected function listMethods(string $type): array
+    {
+        $methods = array_values((new shopPluginModel)->listPlugins($type, ['all' => true]));
+
+        return array_map(function ($m) {
+            return [
+                'id'           => (int)$m['id'],
+                'name'         => (string)$m['name'],
+                'shop_enabled' => (bool)$m['status']
+            ];
+        }, $methods);
+    }
+
+    /**
+     * Актуализируем настройку выбранных методов -- добавляем новые, убираем удалённые
+     *
+     * @param array $setting
+     * @param array $methods
+     * @return array
+     */
+    protected function actualizeSelected(array $setting, array $methods): array
+    {
+        $methods = array_column($methods, 'id', 'id');
+
+        foreach ($setting['selected'] as $index => $item) {
+            if (isset($methods[$item['id']])) {
+                unset($methods['id']);
+                continue;
+            };
+            unset($setting['selected'][$index]);
+        }
+
+        if ($methods) {
+            foreach ($methods as $method) {
+                $setting['selected'][] = ['id' => $method, 'enabled' => false];
+            }
+        }
+
+        $setting['selected'] = array_values($setting['selected']);
+
+        return $setting;
+    }
 }
--- lib/shopDbcPlugin.class.php
+++ lib/shopDbcPlugin.class.php
@@ -11,5 +11,50 @@ declare(strict_types=1);
  */
 class shopDbcPlugin extends shopPlugin
 {
+    protected $typecasted_settings;
 
+    /**
+     * @param null|string|int $name
+     * @return array|mixed|null
+     */
+    public function getSettings($name = null)
+    {
+        if (!$this->typecasted_settings) $this->typecastSettings(parent::getSettings($name));
+
+        if ($name === null) return $this->typecasted_settings;
+
+        return $this->typecasted_settings[$name] ?? null;
+    }
+
+    /**
+     * @param array $settings
+     * @return array
+     */
+    protected function typecastSettings(array $settings): array
+    {
+        if ($this->typecasted_settings) return $this->typecasted_settings;
+
+        foreach ($settings as $key => $setting) {
+            switch ($key) {
+                case 'shipping':
+                case 'payment':
+                    $setting = (array)$setting;
+                    $setting['all'] = boolval($setting['all'] ?? false);
+                    $setting['selected'] = $setting['selected'] ?? [];
+                    if (!$setting['all']) {
+                        foreach ($setting['selected'] as &$item) {
+                            $item['id'] = (int)$item['id'];
+                            $item['enabled'] = (bool)$item['enabled'];
+                        }
+                        unset($item);
+                    }
+                    break;
+            }
+            $settings[$key] = $setting;
+        }
+
+        $this->typecasted_settings = $settings;
+
+        return $this->typecasted_settings;
+    }
 }
