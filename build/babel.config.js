78234a740316fe1f668d3aecdbc90d5353e73e87
Serge Rodovnichenko <sergerod@gmail.com>
2021-07-17 13:30:38 +0300

    Начато приложение настроек

---
+++ build/babel.config.js
@@ -0,0 +1,22 @@
+module.exports = function (api) {
+    const presets = [
+        ["@babel/preset-env", {
+            targets: {
+                ie: "11"
+            },
+            useBuiltIns: false
+        }]
+    ];
+    const plugins = [
+        "@babel/plugin-transform-runtime",
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-proposal-class-properties"
+    ];
+    // const plugins = [];
+
+    api.cache(false);
+
+    return {presets, plugins};
+};
+
+
