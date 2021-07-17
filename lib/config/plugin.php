80158114971d530296e5517627ab90d108883750
Serge Rodovnichenko <sergerod@gmail.com>
2021-07-17 11:52:24 +0300

    Change name

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -1,10 +1,9 @@
 <?php
-return array (
-  'name' => 'Dbc',
-  'img' => 'img/dbc.gif',
-  'version' => '1.0.0',
-  'vendor' => '670917',
-  'handlers' => 
-  array (
-  ),
+return array(
+    'name'     => 'Оплата доставки получателем',
+    'img'      => 'img/dbc.gif',
+    'version'  => '1.0.0',
+    'vendor'   => '670917',
+    'handlers' =>
+        array(),
 );
